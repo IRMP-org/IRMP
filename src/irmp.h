9b3cc72f5dfaf95cbd4d8fc457096052db86a2d0
j1rie <j1rie@users.noreply.github.com>
2026-02-26 17:16:39 +0100

    fix mistake

--- src/irmp.h
+++ src/irmp.h
@@ -343,7 +343,7 @@ extern bool                             irmp_ISR (void);
 extern uint_fast8_t                     irmp_get_data (IRMP_DATA *);
 extern uint_fast8_t                     irmp_ISR (void);
 #endif
-#ifdef IRMP_AUTODETECT_REPEATRATE
+#if IRMP_AUTODETECT_REPEATRATE
 extern volatile uint_fast8_t            delta, min_delta, keep_same_key, timeout, upper_border;
 extern volatile uint_fast16_t           tmp_delta;
 extern volatile uint32_t                pass_on_delta_detection;
--- src/irmp.hpp
+++ src/irmp.hpp
@@ -5509,7 +5509,7 @@ uint_fast8_t irmp_ISR(void)
                 {
                     if (last_irmp_command == irmp_tmp_command &&
                         last_irmp_address == irmp_tmp_address &&
-#ifdef IRMP_AUTODETECT_REPEATRATE
+#if IRMP_AUTODETECT_REPEATRATE
                         irmp_protocol == previous_irmp_protocol)
                     {
                         same_key = 1;
